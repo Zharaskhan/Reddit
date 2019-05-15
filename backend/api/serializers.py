0e12d805a6c7668a00aa0e9ae5744c7cd49567e7
Akbota <bmautkazy@gmail.com>
2019-05-15 16:26:56 +0600

    Add serializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -17,27 +17,50 @@ class UserRegisterSerializer(serializers.ModelSerializer):
 
 
 
-class CommentSerializer(serializers.ModelSerializer):
+class CommentSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     author = UserSerializer(read_only=True)
     created_at = serializers.DateTimeField(read_only=True)
     comment_likes = serializers.ReadOnlyField()
 
-    class Meta:
-        model = Comment
-        fields = ('id', 'author', 'created_at', 'comment_likes', 'text')
+    def create(self, validated_data):
+        comment_list = Comment(**validated_data)
+        comment_list.save()
+        return comment_list
 
-class PostSerializer(serializers.ModelSerializer):
+    def update(self, instance, validated_data):
+        instance.author = validated_data.get('author', instance.author)
+        instance.created_at = validated_data.get('created_at', instance.created_at)
+        instance.comment_likes = validated_data.get('comment_likes', instance.comment_likes)
+        instance.save()
+        return instance
+
+
+class PostSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField()
+    body = serializers.CharField()
     author = UserSerializer(read_only=True)
     created_at = serializers.DateTimeField(read_only=True)
     post_likes = serializers.ReadOnlyField()
     post_comments = serializers.ReadOnlyField()
     comments = CommentSerializer(read_only=True, many=True)
 
-    class Meta:
-        model = Post
-        fields = '__all__'
+    def create(self, validated_data):
+        post_list = Post(**validated_data)
+        post_list.save()
+        return post_list
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.body = validated_data.get('body', instance.body)
+        instance.author = validated_data.get('author', instance.author)
+        instance.created_at = validated_data.get('created_at', instance.created_at)
+        instance.post_likes = validated_data.get('post_likes', instance.post_likes)
+        instance.post_comments = validated_data.get('post_comments', instance.post_comments)
+        instance.save()
+        return instance
+
 
 
 class LikeSerializer(serializers.ModelSerializer):
