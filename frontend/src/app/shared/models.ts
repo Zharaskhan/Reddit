7b0a253725bd667c36979e6a9c0b36b0ce706bd3
Zharaskhan Aman <aman.zharaskhan@gmail.com>
2019-05-15 06:02:10 +0600

    Add comments

--- frontend/src/app/shared/models.ts
+++ frontend/src/app/shared/models.ts
@@ -9,9 +9,21 @@ export interface IPOST {
     created_at: Date;
     title: string;
     body: string;
+    post_likes: number;
+    post_comments: number;
+    comments: ICOMMENT[];
 }
 
 export interface IAuthResponse {
     token: string;
+}
+
+
+export interface ICOMMENT {
+    id: number;
+    author: IUser;
+    created_at: Date;
+    text: string;
+    comment_likes: number;
 }
 
